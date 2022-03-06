c1684fb95a90706787c49ba1e74557336bf11f4a
Aniket <aniketsaste96@gmail.com>
2022-03-06 11:44:18 +0530

    chat user

--- src/components/chatOnline/ChatOnline.jsx
+++ src/components/chatOnline/ChatOnline.jsx
@@ -15,6 +15,28 @@ const ChatOnline = () => {
           </div>
           <span className="chatOnlinen">akshay jadhav</span>
         </div>
+        <div className="chatOnlineFriend">
+          <div className="chatOnlineImgContainer">
+            <img
+              src="http://getwallpapers.com/wallpaper/full/9/7/9/566755.jpg"
+              alt=""
+              className="chatOnlineImg"
+            />
+            <div className="chatOnlineBadge"></div>
+          </div>
+          <span className="chatOnlinen">akshay jadhav</span>
+        </div>
+        <div className="chatOnlineFriend">
+          <div className="chatOnlineImgContainer">
+            <img
+              src="http://getwallpapers.com/wallpaper/full/9/7/9/566755.jpg"
+              alt=""
+              className="chatOnlineImg"
+            />
+            <div className="chatOnlineBadge"></div>
+          </div>
+          <span className="chatOnlinen">akshay jadhav</span>
+        </div>
       </div>
     </>
   );
