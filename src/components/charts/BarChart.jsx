793032e6023eabfa6028be4ea5e953099c3788a8
Aniket <aniketsaste96@gmail.com>
2022-03-09 14:32:28 +0530

    charts

--- src/components/charts/BarChart.jsx
+++ src/components/charts/BarChart.jsx
@@ -1,10 +1,45 @@
 import React from "react";
-import { Bar } from "react-chartjs-2";
+import "./charts.css";
+import { Pdata } from "./chartData.js";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const BarChart = () => {
   return (
     <>
-      <h1>Chart</h1>
+      <div className="chart">
+        <h1 className="charHeading">Active Users & Posts</h1>
+        <ResponsiveContainer width="100%" aspect={3}>
+          <LineChart
+            data={Pdata}
+            width={500}
+            height={300}
+            margin={{ right: 20, left: 20 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+
+            <Line
+              type="monotone"
+              dataKey="users"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            <XAxis dataKey="Month" interval={"preserveStartEnd"} />
+            <YAxis />
+            <Line dataKey="posts" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
     </>
   );
 };
---
+++ src/components/charts/chartData.js
@@ -0,0 +1,38 @@
+export const Pdata = [
+    {
+        Month: "jan",
+        users: "13",
+        posts: "10"
+    },
+    {
+        Month: "feb",
+        users: "15",
+        posts: "09"
+    },
+    {
+        Month: "Mar",
+        users: "22",
+        posts: "12"
+    },
+    {
+        Month: "Apr",
+        users: "13",
+        posts: "15"
+    },
+    {
+        Month: "May",
+        users: "09",
+        posts: "7"
+    },
+    {
+        Month: "June",
+        users: "15",
+        posts: "20"
+    },
+    {
+        Month: "July",
+        users: "20",
+        posts: "22"
+    },
+
+]
